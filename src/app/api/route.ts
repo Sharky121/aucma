570810959a2339f37c2757c2a94ab2bd3088d281
Sharky <loginov@2cherryline.ru>
2024-09-22 20:10:51 +0300

    Попытка настроить почту с сервера

---
+++ src/app/api/route.ts
@@ -0,0 +1,45 @@
+import nodemailer from 'nodemailer';
+
+export async function POST(request: Request, response: Response) {
+    const formData = await request.json();
+
+    const { name, email, message } = formData;
+
+    // Конфигурация для Postmark
+    // const transporter = nodemailer.createTransport({
+    //   host: 'smtp.postmarkapp.com',
+    //   port: 587,
+    //   secure: false, 
+    //   auth: {
+    //     user: '97a9265c-a1b9-4930-876e-c50a4279686f', 
+    //     pass: '97a9265c-a1b9-4930-876e-c50a4279686f'
+    //   }
+    // });
+
+  //   const transporter = nodemailer.createTransport({
+  //     host: 'smtp.mail.ru',
+  //     port: 465,
+  //     secure: true,
+  //     auth: {
+  //         user: 'loginov@cherryline.ru',
+  //         pass: 'k6vwcT5b3QiddxcRMjbT',
+  //     },
+  // });
+
+
+    // Отправляем письмо
+    // const mailOptions = {
+    //   from: 'Ваше Имя <noreply@aucma-rus.ru>', // "Отправитель" - используйте свой собственный домен
+    //   to: 'Sharky121@mail.ru', // Получатель
+    //   subject: 'Новое сообщение из формы обратной связи',
+    //   text: 'test'
+    // };
+
+    // try {
+    //     await transporter.sendMail(mailOptions);
+    // } catch (error) {
+    //     console.error(error);
+    // }
+
+    return Response.json('Ответ с сервера');
+  }
