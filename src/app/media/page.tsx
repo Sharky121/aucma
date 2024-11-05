5d468c92913e0feffd9da3ac0a7a28b6d5fc9cc2
Sharky <loginov@2cherryline.ru>
2024-11-05 18:04:10 +0300

    feat: Добавляет новости

--- src/app/media/page.tsx
+++ src/app/media/page.tsx
@@ -4,45 +4,7 @@ import mediaStyles from "../styles/sections/media/media.module.scss";
 import newsStyles from "../styles/sections/news/news.module.scss";
 import Container from "@/components/container/container";
 import Link from "next/link";
-
-const News = [
-    {
-        "id": 1,
-        "date": "02.09.2024",
-        "title": "При обработке рамы Полуприцепа «AUCMA» теперь используется технология KTL (Kombinierter Rost- und Lackierverfahren)",
-        "desc": "При обработке рамы Полуприцепа «AUCMA» теперь используется технология KTL (Kombinierter Rost- und Lackierverfahren)», а описание к ней с «Специалистами компании AUCMA была разработана и внедрена в производство полностью оцинкованная рама. Метод: горячие цинкование при температуре 400 °С.» на «Этот процесс включает несколько этапов, которые обеспечивают высокую степень защиты от коррозии и внешних воздействий. Технология KTL позволяет значительно увеличить срок службы полуприцепов за счет высокой степени защиты от атмосферных воздействий, механических повреждений и коррозии. Рама Полуприцепа «AUCMA» представляет собой усиленную стальную конструкцию из продольных лонжеронов и поперечных балок, изготовленных из стали повышенной прочности. Конструкция рамы разработана инженерами завода «AUCMA» с учетом особенностей эксплуатации на территории РФ, что позволило снизить массу полуприцепа без потери прочности и жесткости рамы"
-    },
-    {
-        "id": 2,
-        "date": "18.08.2024",
-        "title": "Достигнуто соглашение с компанией Carrier о гарантии на ХОУ в России",
-        "desc": "ООО АКМА РУС заключило договор поставки холодильного оборудования компании Carrier с гарантией на обслуживание. Покупая полуприцеп AUCMA клиенты могут быть спокойны, что гарантийное обсаживание на иностранное  оборудование сохранится."
-    },
-    {
-        "id": 3,
-        "date": "24.07.2024",
-        "title": "Проблемы с платежами в Китай решены",
-        "desc": "Ситуация с расчетами между двумя странами стабилизировалась после поездки президента Владимира Путина в Китай: с Россией стали напрямую работать региональные банки КНР, и возобновил прием платежей один из крупных игроков - Bank of China."
-    },
-    {
-        "id": 4,
-        "date": "30.06.2024",
-        "title": "Компания AUCMA заключила партнерское соглашение с компанией FAW TRUCKS",
-        "desc": "В настоящее время была разработана партнерская программа c FAW TRUCKS, которая позволяет приобрести сцепку (тягач + полуприцеп) по уникальной цене."
-    },
-    {
-        "id": 5,
-        "date": "17.05.2024",
-        "title": "Испытания Полуприцепа AUCMA подтвердили европейское качество",
-        "desc": "Полуприцеп AUCMA завершил испытания по подтверждению качества продукции. По результатам было установлено, что качество кузова не уступает ведущим брендам KRONE SCHMITZ."
-    },
-    {
-        "id": 6,
-        "date": "05.04.2024",
-        "title": "Легкость в движении, надежность в пути!",
-        "desc": "История создания официального слогана компании. Сложности внедрения и административные барьеры."
-    }
-];
+import News from "@/data/news";
 
 export default async function Page() {
     const COUNT_TRIM_TEXT = 233;
@@ -75,11 +37,11 @@ export default async function Page() {
                 <Container customClassName={newsStyles.newsContainer}>
                     <ul className={newsStyles.newsList}>
                         {
-                            News.map(({id, title, date, desc}) => (
+                            News.splice(0, 6).map(({id, title, date, desc}) => (
                                 <li key={id} className={newsStyles.newsItem}>
                                     <h3 className={newsStyles.newsTitle}>{title}</h3>
                                     <time className={newsStyles.newsDate} dateTime="2015-11-18">{date}</time>
-                                    <p className={newsStyles.newsDesc}>{trimDesc(desc)}</p>
+                                    <p className={newsStyles.newsDesc} dangerouslySetInnerHTML={{__html: trimDesc(desc) ?? ''}}/>
                                     <Link className={newsStyles.newsMore} href={`/media/${id}`}>
                                         <span>Подробнее</span>
                                         <svg viewBox="0 0 22 22" width="16" height="16" aria-hidden="true" focusable="false">
--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -12,22 +12,23 @@ import styles from "./map.module.scss";
 
 const location = { center: [37.583760, 55.381915], zoom: 15 };
 
-const Map = () => {
-
-    return (
-        <>
-            <div className={styles.mapContainer}>
-                <YMapComponentsProvider apiKey='cac5b520-ec6d-4f80-b8a5-eb55d0da1f0a'>
-                    <YMap location={location}>
-                        <YMapDefaultSchemeLayer />
-                        <YMapDefaultFeaturesLayer />
-                        <YMapMarker coordinates={[55.376559, 37.588183]}/>
-                        {/* <YMapMarker coordinates={[55.376757, 37.591939]}/> */}
-                    </YMap>
-                </YMapComponentsProvider>
-            </div>        
-        </>
-    )
-}
+const Map = () => (
+    <>
+        <div className={styles.mapContainer}>
+            <YMapComponentsProvider apiKey='cac5b520-ec6d-4f80-b8a5-eb55d0da1f0a'>
+                <YMap location={location}>
+                    <YMapDefaultSchemeLayer />
+                    <YMapDefaultFeaturesLayer />
+                    <YMapMarker
+                        coordinates={[37.583760, 55.381915]}
+                        source="marker.png" 
+                    />
+                    <YMapDefaultMarker coordinates={[37.583760, 55.381915]}/>
+                    <YMapDefaultMarker coordinates={[37.583760, 55.381920]}/>
+                </YMap>
+            </YMapComponentsProvider>
+        </div>
+    </>
+)
 
 export default Map;
