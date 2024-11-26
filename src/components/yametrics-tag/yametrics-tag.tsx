4329655a492c5b2d513bd596ac1f6171c58d32ca
Sharky <loginov@2cherryline.ru>
2024-11-26 11:46:59 +0300

    fix: Добавляет счётчик по новому

---
+++ src/components/yametrics-tag/yametrics-tag.tsx
@@ -0,0 +1,12 @@
+const YandexMetrikaTag = () => {
+    return (
+      <>
+          <noscript>
+            <div><img src="https://mc.yandex.ru/watch/98962211" style={{ position: "absolute", left: "-9999px" }} alt="" /></div>
+          </noscript>
+      </>
+    );
+  };
+  
+export default YandexMetrikaTag;
+  
