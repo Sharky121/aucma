d59ae1cac46e31614e6b725ab4e81d23b5b629ee
Sharky <loginov@2cherryline.ru>
2025-02-11 12:23:37 +0300

    feat: Добавляет новые точки на карту

---
+++ src/components/map/data.ts
@@ -0,0 +1,367 @@
+export const Points = [
+    {
+        id: "0",
+        coordinates: [37.583760, 55.381915],
+        properties: {
+            hint: {
+                title: 'ООО «Траклайн»',
+                content: `
+                    <dl>
+                        <div>
+                            <dt>Адрес:</dt>
+                            <dd>МО, г.о. Подольск, д. Коледино, 30А</dd>
+                        </div>
+                        <div>
+                            <dt>Телефон:</dt>
+                            <dd>+7 (499) 677-65-43</dd>
+                        </div>
+                    </dl>
+                `
+            }
+        }
+    },
+    {
+        id: "1",
+        coordinates: [39.027268, 45.135706],
+        properties: {
+            hint: {
+                title: 'ООО «ЮРТО-ТРАК»',
+                content: `
+                    <dl>
+                        <div>
+                            <dt>Адрес:</dt>
+                            <dd>350010, г. Краснодар, ул. Ростовское шоссе, д. 47</dd>
+                        </div>
+                        <div>
+                            <dt>Телефон:</dt>
+                            <dd>+7 (928) 404-90-20, +7 (861) 225-93-75</dd>
+                        </div>
+                        <div>
+                            <dt>Сайт:</dt>
+                            <dd><a href="https://urto-truck.ru/">https://urto-truck.ru</a></dd>
+                        </div>
+                    </dl>
+                `
+            }
+        }
+    },
+    {
+        id: "2",
+        coordinates: [82.904788, 55.032731],
+        properties: {
+            hint: {
+                title: 'ООО «Сибирские прицепы»',
+                content: `
+                    <dl>
+                        <div>
+                            <dt>Адрес:</dt>
+                            <dd>630004, г. Новосибирск, ул. Комсомольский проспект, 13/1, оф. 503</dd>
+                        </div>
+                        <div>
+                            <dt>Телефон:</dt>
+                            <dd>+7 (913) 013- 11-69, +7(913) 200-00-24</dd>
+                        </div>
+                        <div>
+                            <dt>Сайт:</dt>
+                            <dd><a href="https://sibtrailers.ru">https://sibtrailers.ru</a></dd>
+                        </div>
+                        <div>
+                            <dt>Email:</dt>
+                            <dd>sergboss12@mail.ru, siberiantrailers@mail.ru</dd>
+                        </div>
+                    </dl>
+                `
+            }
+        }
+    },
+    {
+        id: "3",
+        coordinates: [50.257255, 53.275143],
+        properties: {
+            hint: {
+                title: 'ГК ВОЛГАКОМТРАНС',
+                content: `
+                    <dl>
+                        <div>
+                            <dt>Адрес:</dt>
+                            <dd>г. Самара, Волжское шоссе 108</dd>
+                        </div>
+                        <div>
+                            <dt>Телефон:</dt>
+                            <dd>8 (800) 201-96-99, +7 (927) 760-26-03</dd>
+                        </div>
+                        <div>
+                            <dt>Сайт:</dt>
+                            <dd><a href="https://www.volgakomtrans.ru">Сайт</a></dd>
+                        </div>
+                        <div>
+                            <dt>Email:</dt>
+                            <dd>info@volgakomtrans.ru</dd>
+                        </div>
+                    </dl>
+                `
+            }
+        }
+    },
+    {
+        id: "4",
+        coordinates: [83.597265, 53.351886],
+        properties: {
+            hint: {
+                title: 'BIG MASHINE',
+                content: `
+                    <dl>
+                        <div>
+                            <dt>Адрес:</dt>
+                            <dd>г.Барнаул, Павловский тракт, 200г</dd>
+                        </div>
+                        <div>
+                            <dt>Телефон:</dt>
+                            <dd>8 (800) 700-15-09, +7 (961) 985-31-68</dd>
+                        </div>
+                        <div>
+                            <dt>Сайт:</dt>
+                            <dd><a href="https://bigmachine.pro">Сайт</a></dd>
+                        </div>
+                        <div>s
+                            <dt>Email:</dt>
+                            <dd>n.valeev@bigm.pro</dd>
+                        </div>
+                    </dl>
+                `
+            }
+        }
+    },
+    {
+        id: "5",
+        coordinates: [37.188654, 56.004756],
+        properties: {
+            hint: {
+                title: 'ООО "Глобал Трак Сейлс"',
+                content: `
+                    <dl>
+                        <div>
+                            <dt>Адрес:</dt>
+                            <dd>г. Москва, Зеленоград, 2-й Западный проезд, 3c2</dd>
+                        </div>
+                        <div>
+                            <dt>Телефон:</dt>
+                            <dd>8 800 350-58-32</dd>
+                        </div>
+                        <div>
+                            <dt>Сайт:</dt>
+                            <dd><a href="https://www.gt-sales.ru">Сайт</a></dd>
+                        </div>
+                        <div>s
+                            <dt>Email:</dt>
+                            <dd>info@gt-sales.ru</dd>
+                        </div>
+                    </dl>
+                `
+            }
+        }
+    },
+    {
+        id: "6",
+        coordinates: [30.170134, 59.794645], 
+        properties: {
+            hint: {
+                title: 'ООО "Глобал Трак Сейлс"',
+                content: `
+                    <dl>
+                        <div>
+                            <dt>Адрес:</dt>
+                            <dd>Ленинградская область, Ломоносовский район, Волхонское шоссе, д.5</dd>
+                        </div>
+                        <div>
+                            <dt>Телефон:</dt>
+                            <dd>8 (812) 245-25-69</dd>
+                        </div>
+                        <div>
+                            <dt>Сайт:</dt>
+                            <dd><a href="https://www.gt-sales.ru">Сайт</a></dd>
+                        </div>
+                        <div>s
+                            <dt>Email:</dt>
+                            <dd>spb@gt-sales.ru</dd>
+                        </div>
+                    </dl>
+                `
+            }
+        }
+    },
+    {
+        id: "7",
+        coordinates: [60.759063, 56.753054], 
+        properties: {
+            hint: {
+                title: 'ООО "Глобал Трак Сейлс"',
+                content: `
+                    <dl>
+                        <div>
+                            <dt>Адрес:</dt>
+                            <dd>Екатеринбург, ул. Черняховского, 92</dd>
+                        </div>
+                        <div>
+                            <dt>Телефон:</dt>
+                            <dd>8 (343) 224-10-21</dd>
+                        </div>
+                        <div>
+                            <dt>Сайт:</dt>
+                            <dd><a href="https://www.gt-sales.ru">Сайт</a></dd>
+                        </div>
+                        <div>s
+                            <dt>Email:</dt>
+                            <dd>ekb@gt-sales.ru</dd>
+                        </div>
+                    </dl>
+                `
+            }
+        }
+    },
+    {
+        id: "8",
+        coordinates: [44.078930, 56.221660],
+        properties: {
+            hint: {
+                title: 'ООО "Глобал Трак Сейлс"',
+                content: `
+                    <dl>
+                        <div>
+                            <dt>Адрес:</dt>
+                            <dd>Нижегородская обл., пос. Ждановский, ул. Придорожная, 20</dd>
+                        </div>
+                        <div>
+                            <dt>Телефон:</dt>
+                            <dd>8 (831) 215-02-26</dd>
+                        </div>
+                        <div>
+                            <dt>Сайт:</dt>
+                            <dd><a href="https://www.gt-sales.ru">Сайт</a></dd>
+                        </div>
+                        <div>s
+                            <dt>Email:</dt>
+                            <dd>nnov@gt-sales.ru</dd>
+                        </div>
+                    </dl>
+                `
+            }
+        }
+    },
+    {
+        id: "9",
+        coordinates: [38.834646, 44.827331], 
+        properties: {
+            hint: {
+                title: 'ООО "Глобал Трак Сейлс"',
+                content: `
+                    <dl>
+                        <div>
+                            <dt>Адрес:</dt>
+                            <dd>Краснодарский край, Северский р-н, пгт. Афипский, ул. Магистральная, д. 14</dd>
+                        </div>
+                        <div>
+                            <dt>Телефон:</dt>
+                            <dd>8 (861) 211-62-80</dd>
+                        </div>
+                        <div>
+                            <dt>Сайт:</dt>
+                            <dd><a href="https://www.gt-sales.ru">Сайт</a></dd>
+                        </div>
+                        <div>s
+                            <dt>Email:</dt>
+                            <dd>krd@gt-sales.ru</dd>
+                        </div>
+                    </dl>
+                `
+            }
+        }
+    },
+    {
+        id: "10",
+        coordinates: [82.809946, 54.921163],
+        properties: {
+            hint: {
+                title: 'ООО "Глобал Трак Сейлс"',
+                content: `
+                    <dl>
+                        <div>
+                            <dt>Адрес:</dt>
+                            <dd>Новосибирская область, Новосибирский район, Верх-Тулинский сельсовет, поселок Красный Восток, ул. Советская, 71А</dd>
+                        </div>
+                        <div>
+                            <dt>Телефон:</dt>
+                            <dd>8 (383) 285-95-04</dd>
+                        </div>
+                        <div>
+                            <dt>Сайт:</dt>
+                            <dd><a href="https://www.gt-sales.ru">Сайт</a></dd>
+                        </div>
+                        <div>s
+                            <dt>Email:</dt>
+                            <dd>nsk@gt-sales.ru</dd>
+                        </div>
+                    </dl>
+                `
+            }
+        }
+    },
+    {
+        id: "11",
+        coordinates: [39.832117, 47.282820],
+        properties: {
+            hint: {
+                title: 'ООО "Глобал Трак Сейлс"',
+                content: `
+                    <dl>
+                        <div>
+                            <dt>Адрес:</dt>
+                            <dd>Ростовская обл., г. Аксай, Аксайский проспект 13Б</dd>
+                        </div>
+                        <div>
+                            <dt>Телефон:</dt>
+                            <dd>8 (863) 333-96-38</dd>
+                        </div>
+                        <div>
+                            <dt>Сайт:</dt>
+                            <dd><a href="https://www.gt-sales.ru">Сайт</a></dd>
+                        </div>
+                        <div>s
+                            <dt>Email:</dt>
+                            <dd>info@gt-sales.ru</dd>
+                        </div>
+                    </dl>
+                `
+            }
+        }
+    },
+    {
+        id: "12",
+        coordinates: [117.327010, 49.651640], 
+        properties: {
+            hint: {
+                title: 'Площадка ООО "АКМА РУС"',
+                content: `
+                    <dl>
+                        <div>
+                            <dt>Адрес:</dt>
+                            <dd>Забайкальский край, Район Забайкальский, пгт. Забайкальск, Территория Производственная база номер 5</dd>
+                        </div>
+                        <div>
+                            <dt>Телефон:</dt>
+                            <dd>8 (499) 677-65-43</dd>
+                        </div>
+                        <div>
+                            <dt>Сайт:</dt>
+                            <dd><a href="https://www.aucma-rus.ru">Сайт</a></dd>
+                        </div>
+                        <div>s
+                            <dt>Email:</dt>
+                            <dd>sales@aucma-rus.ru</dd>
+                        </div>
+                    </dl>
+                `
+            }
+        }
+    }
+];
